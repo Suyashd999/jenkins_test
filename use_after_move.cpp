ce1aa9c0978e263fbcc87ccb130f3760253ca85d
Suyash Dongre <109069262+Suyashd999@users.noreply.github.com>
2024-06-30 14:38:09 +0530

    Update use_after_move.cpp

--- use_after_move.cpp
+++ use_after_move.cpp
@@ -8,7 +8,7 @@ void useAfterMove() {
 
     // str is now in a valid but unspecified state
     std::cout << "Moved string: " << movedStr << std::endl;
-
+    std::cout << "Hi" << std::endl;
     // Attempting to use str after it has been moved is a use after move problem
     // Uncommenting the following line can lead to undefined behavior
     // std::cout << "Original string after move: " << str << std::endl;
